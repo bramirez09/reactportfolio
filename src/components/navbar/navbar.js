bbc81454cd7525dbdabb5e33e01b05246b5fb367
brecec <brendaramirez@brendas-mbp.lan>
2023-09-12 21:37:42 -0500

    added nav tabs

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './navbar.css';
+import { Link } from 'react-scroll' ; 
 
 const Navbar = () => {
     return (
-        <div className="nav-container">navbar</div>
+        <nav className="navbar">
+            <img src ="" alt = ""/>
+                <div className = "desktopMenu">
+                    <Link className = "desktopMenuListItem">About Me</Link>
+                    <Link className = "desktopMenuListItem">Portfolio</Link>
+                    <Link className = "desktopMenuListItem">Contact</Link>
+                    <Link className = "desktopMenuListItem">Resume</Link>
+                </div>
+                <button className = "desktopMenuBtn">
+                    <img src = "" alt= "" className = "desktopMenuImg"/>Contact Me</button>
+        </nav>
     )
 }
 
