73321418b285e611a1ed4249c48c59cebe2be40e
brecec <brendaramirez@Brendas-MacBook-Pro.local>
2023-09-16 10:40:15 -0500

    added bio

--- src/components/aboutme/aboutme.js
+++ src/components/aboutme/aboutme.js
@@ -6,9 +6,13 @@ import Avatar from '../../assets/AvatarMaker.png';
 const Aboutme = () => {
     return (
         <section id = "aboutme">
-            <div className = "avatar">
+            <div className = "aboutme">
                 <img src={Avatar} className = "avatar"/>
-                <span className = "hi">Hi!</span>
+                <span className = "hi" id = "intro">Hello!</span>
+                <p className = "name" id = "intro">My name is Brenda</p>
+                <p className = "bio" id = "intro">
+                    I have 10 years experience working in a tech company. I have my bachelors in Business Administration from Texas Womens University and a certificate from UTSA in FullStack Web Development. I am currently a Senior QA tester for an agtech company with a background in implementations and software solutions making me efficient in understanding, implementing and testing software as a service applications. I am passionate about learning and expanding my skills sets.
+                </p>
             </div>
         </section>
             
