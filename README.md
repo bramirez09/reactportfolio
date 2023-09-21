238d6d853fcc503b1867fb1ef28ef8364be089fa
brecec <brendaramirez@brendas-mbp.lan>
2023-09-20 22:16:20 -0500

    readme and validate email

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # reactportfolio
+
+## Description
+This is my portfolio website. It contains information about me, projects I have worked on and contact details for people to reach out with questions or comments.
+
+## Table of Contents
+* [Visuals](#visuals)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Resources](#resources)
+* [Questions](#questions)
+
+## Visuals
+![Alt text](src/assets/portfoliodemo.png)
+
+## Installation
+npm install, npm start
+
+## Usage
+This is a portfolio for my projects and resume.
+
+## Resources
+https://reactjs.org/, https://www.npmjs.com/package/@material-ui/core, https://github.com/mui 
+
+## Questions
+For any questions, please contact me @ 
+https://github.com/bramirez09
+or
+Email: bramirez09@icloud.com
--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,26 +1,58 @@
 import React from 'react';
+import { useState } from 'react';
 import './contact.css';
-import {validateEmail} from '../../utils/helper'
+import { validateEmail } from '../../utils/helper'
+import Resume from '../../assets/resumedemo.png'
 
 const Contact = () => {
-    // const [email,setEmail] = UseState('');
+    const [email, setEmail] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
-    // const handleFormSubmit = (e) => {
-    //     if (!validateEmail(email)) {
-    //         setErrorMessage('Email is invalid');
-    // }
+    const handleFormSubmit = (e) => {
 
+        e.preventDefault()
+        console.log("email", email)
+
+    }
+    const handleInputChange = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+        console.log(inputValue, inputType)
+
+        // Based on the input type, we set the state of either email, username, and password
+        if (inputType === 'email') {
+            if (!validateEmail(inputValue)) {
+                setErrorMessage('Email is invalid');
+            } else {
+                setErrorMessage('')
+                setEmail(inputValue);
+            }
+        }
+        // else if (inputType === 'userName') {
+        //   setUserName(inputValue);
+        // } else {
+        //   setPassword(inputValue);
+        // }
+    };
     return (
-        < section className = "contactPage" >
-            <div className = "contact">
+        < section className="contactPage" >
+            <div className="contact">
                 <h2 className='contacttitle'>Contact Me</h2>
                 <span className='contactDesc'> Please Fillout Form</span>
-                <form className='contactForm'>
+                <form className='contactForm' onSubmit={handleFormSubmit}>
                     <input type="text" className='name' placeholder="<NAME>" />
-                    <input type="text" className='email' placeholder='<Email>'/>
-                    <textarea name="message" rows="5" placeholder='<Your message here>'/>
+                    <input type="email" name="email" className='email' onChange={handleInputChange} placeholder='<Email>' />
+                    {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                    <textarea name="message" rows="5" placeholder='<Your message here>' />
                     <button type='submit' value='Send' className='submitBtn'>Submit</button>
                 </form>
+                <a href={Resume} download={Resume}>Resume</a>
             </div>
         </section>
     )
